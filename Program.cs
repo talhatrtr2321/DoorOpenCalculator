008105126174ecd904e5e298082661256ba3f342
Talhatatar <talhatatar214@gmail.com>
2026-05-26 20:30:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace fistproject1234
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("NOT:Bu program düzgün çalışmayabilir!!!");
+
+            //1.SAYIYI GİR
+            Console.Write("Birinci sayıyı gir:");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+            //2.SAYIYI GİR
+            Console.Write("ikinci sayıyı gir:");
+            Double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+            //TOPLAMA 
+            double sonuc = sayi1 + sayi2;
+
+
+            //SONUCU YAZDIR
+            Console.WriteLine("Sonuç:" + sonuc); 
+
+            Console.ReadLine();
+        }
+    }
+}
