06a552cf5be62db9dd0de9f377ba9025694151a2
Talhatatar <talhatatar214@gmail.com>
2026-05-27 13:59:02 +0300

    "Hesap makinesi uygulamasına çıkarma özelliği eklendi".

--- fistproject1234/Program.cs
+++ fistproject1234/Program.cs
@@ -16,10 +16,32 @@
 
             //TOPLAMA 
             double sonuc = sayi1 + sayi2;
+             
 
 
             //SONUCU YAZDIR
-            Console.WriteLine("Sonuç:" + sonuc); 
+            Console.WriteLine("Sonuç:" + sonuc);
+
+            Console.Write("Çıkarma");
+
+            //1.SAYIYI GİR
+            Console.Write("Birinci sayıyı gir:");
+            double sayi3 = Convert.ToDouble(Console.ReadLine());
+
+            //2.SAYIYI GİR
+            Console.Write("İkinci sayıyı gir");
+            double sayi4 = Convert.ToDouble(Console.ReadLine());
+
+            //ÇIKARMA
+            double sonuc2 = sayi3 - sayi4;
+
+            //SONUCU YAZDIR
+            Console.WriteLine("Sonuç:" + sonuc2);
+
+
+
+
+
 
             Console.ReadLine();
         }
