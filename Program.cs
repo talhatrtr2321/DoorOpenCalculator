858e06bf37ccf74216093cb7142f4f65d39c32e8
talhatrtr2321 <Talhatatar214@gmail.com>
2026-06-12 11:42:14 +0300

    Proje dosyalari güncellendi

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.Design;
+
+namespace DoorOpenCalculator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+
+            //TOPLAMA
+
+
+            //1.SAYIYI GİR
+            Console.Write("Birinci sayıyı gir:");
+            double sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            //2.SAYIYI GİR
+            Console.Write("ikinci sayıyı gir:");
+            Double sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            //TOPLAMA 
+            double sonuc = sayi1 + sayi2;
+
+            //SONUCU YAZDIR
+            Console.WriteLine("Sonuç:" + sonuc);
+
+
+            //ÇIKARTMA
+
+
+            //1.SAYIYI GİR
+            Console.Write("Birinci sayıyı gir:");
+            double sayi3 = Convert.ToInt32(Console.ReadLine());
+
+            //2.SAYIYI GİR
+            Console.Write("ikinci sayıyı gir:");
+            double sayi4 = Convert.ToInt32(Console.ReadLine());
+
+            //ÇIKARMA
+            double sonuc2 = sayi3 - sayi4;
+
+            //SONUCU YAZDIR
+            Console.WriteLine("sonuç:" + sonuc2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+<img width="512" height="512" alt="calculator-icon-2" src="https://github.com/user-attachments/assets/6d9a2625-373a-4389-9852-2ead37fdd22c" />
+
+# DOOROPENCALCULATOR
+Bu uygulama bir hesap makinesi uygulamasıdır ANCAK sadece toplama yapabiliyor.
+ #### İNDİRMEK İÇİN RELEASE,İN ALTINDA 1.0V,a tıklayın
+
+# KULLANDIĞIM ARAÇLAR 
+
+Visual Studio,
+.NET
+# ÖNEMLİ NOT!!!!
+MALESEF Kİ SAYILARI GİRİP SONUCU GÖRDÜKTEN SONRA BİŞİLER YAZARSANIZ ÇÖKÜYOR ANCAK TEKRAR AÇARSANIZ UYGULAMAYI TEKRAR ESKİ HALİNE GELİYOR.
+
+İNDİRİP EXEYİ AÇTIĞINIZDA AŞAĞIDAKİ EKRANA GELİCEKSİNİZ:
+
+<img width="1919" height="1031" alt="Screenshot 2026-05-27 122811" src="https://github.com/user-attachments/assets/12a23077-1642-4449-8adb-8ff5e5ff8765" />
